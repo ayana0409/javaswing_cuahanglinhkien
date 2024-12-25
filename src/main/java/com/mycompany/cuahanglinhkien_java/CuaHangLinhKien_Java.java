9323df1664f477f32f8dd54f43cf291ada7c6bfc
ayana0409 <duongdoanthuan2003@gmail.com>
2024-12-25 14:37:12 +0700

    init project

---
+++ src/main/java/com/mycompany/cuahanglinhkien_java/CuaHangLinhKien_Java.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cuahanglinhkien_java;
+
+import com.mycompany.cuahanglinhkien_java.views.frmLogin;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Asus
+ */
+public class CuaHangLinhKien_Java {
+
+    public static void main(String[] args) {
+        frmLogin frmLogin = new frmLogin();
+        
+        frmLogin.setVisible(true);
+    }
+}
---
+++ src/main/java/share/GenericController.java
@@ -0,0 +1,82 @@
+package share;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class GenericController<T> {
+    private static final String URL = "jdbc:mysql://localhost:3306/libarymanage";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123123";
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public int insert(String query, Object... params) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            setParameters(stmt, params);
+            int rowsAffected = stmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        return generatedKeys.getInt(1);
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    public int updateOrDelete(String query, Object... params) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            setParameters(stmt, params);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public T fetchOne(String query, Function<ResultSet, T> mapper, Object... params) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            setParameters(stmt, params);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return mapper.apply(rs);
+                }
+                return null;
+            }
+        }
+    }
+
+    public List<T> fetchAll(String query, Function<ResultSet, T> mapper, Object... params) throws SQLException {
+        List<T> results = new ArrayList<>();
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            setParameters(stmt, params);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(mapper.apply(rs)); 
+                }
+            }
+        }
+        return results;
+    }
+
+    private void setParameters(PreparedStatement stmt, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            stmt.setObject(i + 1, params[i]);
+        }
+    }
+}
