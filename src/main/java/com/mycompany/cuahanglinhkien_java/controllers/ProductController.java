0d805a39b078c2f4805ebead2176aab5b4dc5359
thuyhuynh269 <thuyhuynh7562@gmail.com>
2024-12-28 20:45:47 +0700

    add getAllPro

---
+++ src/main/java/com/mycompany/cuahanglinhkien_java/controllers/ProductController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cuahanglinhkien_java.controllers;
+
+import com.mycompany.cuahanglinhkien_java.models.Product;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import share.GenericController;
+/**
+ *
+ * @author DLCT
+ */
+public class ProductController {
+    GenericController<Product> _dbHelper = new GenericController<>();
+
+    public Product getProductById(int id){
+        String query = "SELECT * FROM product WHERE id = ?";
+        try {
+            return _dbHelper.fetchOne(query, rs -> {
+                try {
+                    return new Product(rs.getInt("id"), rs.getString("name"), rs.getInt("categoryId"),
+                            rs.getInt("manufacturerId"),rs.getInt("quantity"),
+                            rs.getString("details"),rs.getString("image"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }, id);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+
+    public List<Product> getAllProduct(){
+        String query = "SELECT * FROM product";
+        try {
+            return _dbHelper.fetchAll(query, rs -> {
+                try {
+                    return new Product(rs.getInt("id"), rs.getString("name"), rs.getInt("categoryId"),
+                            rs.getInt("manufacturerId"),rs.getInt("quantity"),
+                            rs.getString("details"),rs.getString("image"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            });
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+}
+}
+
