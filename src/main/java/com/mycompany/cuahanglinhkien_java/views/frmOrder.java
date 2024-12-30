925094092e291da16487184fb6e69cf74abb70ea
yepvy <yenvy.2342@gmail.com>
2024-12-30 07:32:12 +0700

    frmOrder

--- src/main/java/com/mycompany/cuahanglinhkien_java/views/frmOrder.java
+++ src/main/java/com/mycompany/cuahanglinhkien_java/views/frmOrder.java
@@ -8,12 +8,12 @@ package com.mycompany.cuahanglinhkien_java.views;
  *
  * @author YEN VY
  */
-public class frmProduct extends javax.swing.JFrame {
+public class frmOrder extends javax.swing.JFrame {
 
     /**
      * Creates new form frmProduct
      */
-    public frmProduct() {
+    public frmOrder() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class frmProduct extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmOrder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmOrder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmOrder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmOrder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmProduct().setVisible(true);
+                new frmOrder().setVisible(true);
             }
         });
     }
