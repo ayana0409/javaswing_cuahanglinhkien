6e242c6080f727af184cf0b5d8e004c78fe2c974
ayana0409 <duongdoanthuan2003@gmail.com>
2025-01-05 21:15:21 +0700

    change home open frm order

--- src/main/java/com/mycompany/cuahanglinhkien_java/views/frmHome.java
+++ src/main/java/com/mycompany/cuahanglinhkien_java/views/frmHome.java
@@ -318,9 +318,9 @@ public class frmHome extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnManageOrderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageOrderActionPerformed
-        frmOrderDetails frmOrderDetails = new frmOrderDetails();
-        frmOrderDetails.setLocationRelativeTo(null);
-        frmOrderDetails.setVisible(true);
+        frmOrder frmOrder = new frmOrder();
+        frmOrder.setLocationRelativeTo(null);
+        frmOrder.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btnManageOrderActionPerformed
 
